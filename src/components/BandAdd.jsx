7c677a2c56027cdee83d38471183b82b035b04ea
Nebura <hamklieneduprofesional@gmail.com>
2022-04-15 20:21:48 -0600

    Se finalizo la aplicacion de votacion de bandas

---
+++ src/components/BandAdd.jsx
@@ -0,0 +1,33 @@
+import React, { useContext, useState } from 'react';
+
+import { SocketContext } from '../context/SocketContext';
+
+export const BandAdd = () => {
+
+  const [valor, setValor] = useState('');
+
+  const { socket } = useContext(SocketContext);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (valor.trim().length > 0) {
+      socket.emit('nueva-banda', { nombre: valor });
+      setValor('');
+    }
+  }
+
+  return (
+    <>
+      <h3>Agregar banda</h3>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          className='form-control'
+          placeholder='Nuevo nombre de banda'
+          value={valor}
+          onChange={(event) => setValor(event.target.value)}
+        />
+      </form>
+    </>
+  )
+}
